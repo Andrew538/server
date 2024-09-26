6efe61e511c481bb42e7ab98249ff2d30414a480
Andrew538 <andrew742183@mail.ru>
2024-09-26 21:12:47 +0500

    add cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const PORT = process.env.PORT || 7000
 
 const app = express()
-app.use(cors())
+app.use('*', cors())
 app.use(express.json())
 app.use('/api', router)
 app.use(errorHandler)
