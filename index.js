b0ab1a6f15d117e64ba9be2aac9f6fd253ff70c2
Andrew538 <andrew742183@mail.ru>
2024-10-03 21:07:13 +0500

    i

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const https = require('https');
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 7000
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 app.use(errorHandler)
-
+https 
+.createServer(app)
 const start = async () => {
     try {
         await sequelize.authenticate()
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,10 +5,11 @@ const authMiddleware = require('../middleware/authMiddleware')
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 // const checkAuth = require('../middleware/authMiddleware')
 
-router.post('/registration', checkRoleMiddleware('ADMIN'), userController.registration)
+router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth', authMiddleware, userController.check)
 
+// router.post('/registration', checkRoleMiddleware('ADMIN'), userController.registration)
 
 
 
